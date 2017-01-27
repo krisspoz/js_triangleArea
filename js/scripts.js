c267ab17840ea509cd5b3c90c4f25ad95469761c
Krzysztof Stankowski <Krzysztof Stankowski>
2017-01-27 19:59:59 +0100

    add java script

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// plik scripts.js
+
+var a = prompt('Podaj długość podstawy trójkąta');
+alert('Długość podstawy trójkąta wynosi, ' + a);
+console.log('Długość podstawy trójkąta wynosi, ' + a);
+var h = prompt('Podaj wysokość trójkąta');
+alert('Wysokość trójkąta wynosi, ' + h);
+console.log('Wysokość trójkąta wynosi, ' + h);
+var triangleArea = a*h/2
+console.log(triangleArea);
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
